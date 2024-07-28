838308f7976d6a73c724afc45cdcfebd9a802833
Jun Li <junchenli423@gmail.com>
2024-07-28 07:04:56 -0400

    calcualting finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,91 @@
 import './App.css';
 import calculatorCreator from './logic/main';
 import digit from './components/digit';
+import { useState } from 'react';
 
 function App() {
   let total = calculatorCreator()
   let myValue = total.useTheFunctions(10, 3, 'add')
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [operator, setOperator] = useState('')
+  const [result, setResult] = useState('')
 
-  const numPads = []
-  for(let i = 0; i < 9; i++){
-    numPads.push(digit(i+1, () => {return 'hi'}))
+  function doNumbers(value, type){
+    if(operator=='' && type == 'number'){
+      let holder = num1
+      holder = holder.split('')
+      holder.push(value)
+      holder = holder.join('')
+      setNum1(holder)
+    }
+    else if(operator == "" && type == 'operator'){
+      switch(value){
+        case "/":
+          setOperator("/")
+          break;
+        case "+":
+          setOperator("+")
+          break;
+        case "-":
+          setOperator("-")
+          break;
+        case "*":
+          setOperator("*")
+          break;
+      }
+    } 
+    else if(operator !== '' && type == 'number'){
+      let holder = num2
+      holder = holder.split('')
+      holder.push(value)
+      holder = holder.join('')
+      setNum2(holder)
+    }
+    else if(operator !== '' && num2 != '' && value == '='){
+      let firstNum = Number(num1)
+      let secondNum = Number(num2)
+      let localTotal = 0
+
+      switch(operator){
+        case '/':
+          localTotal = firstNum/secondNum
+          break;
+        case '+':
+          localTotal = firstNum + secondNum
+          break;
+        case '-':
+          localTotal = firstNum - secondNum
+          break;
+        case '*':
+          localTotal = firstNum * secondNum
+          break;
+      }
+      setResult(localTotal)
+    }
   }
+  const numPads = []
+
+  for(let i = -1; i < 9; i++){
+    numPads.push(digit(i+1, doNumbers, 'number'))
+  }
+
+  numPads.push(digit('/', doNumbers, 'operator'))
+  numPads.push(digit('*', doNumbers, 'operator'))
+  numPads.push(digit('+', doNumbers, 'operator'))
+  numPads.push(digit('-', doNumbers, 'operator'))
+  numPads.push(digit('.', doNumbers, 'number'))
+  numPads.push(digit('=', doNumbers, 'finish'))
+
+  console.log(num1, operator, num2)
   return (
     <div className="App">
       <div className='container'>
         {numPads}
+        {operator}
+        {num1}
+        {num2}
+        <h1>Result: {result}</h1>
       </div>
     </div>
   );
--- src/components/digit.js
+++ src/components/digit.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./components.css"
 
-const digit = (value, updateFunction) => {
+const digit = (value, updateFunction, type) => {
     //value for which digit this digit component will be
     //update function will update the parent component's value with the given value of the component on press
-
+    function helperFunction(){
+        updateFunction(value, type)
+    }
     return(
-        <div className = 'digit' onClick={updateFunction(value)}>{value}</div>
+        <div className = 'digit' onClick={helperFunction}>{value}</div>
     )
 }
 
